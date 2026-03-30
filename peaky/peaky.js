f0259627668bafae24159e243298ca905bcfbfda
youssef <youssefbelkassem43@gmail.com>
2026-03-30 06:38:06 +0100

    update

--- peaky/peaky.js
+++ peaky/peaky.js
@@ -40,5 +40,49 @@
 
 // window.onload = typeEffect;
 
+let container = document.querySelector(".episodes");
 
+function renderEpisodes(seasons, seasonId) {
+  container.innerHTML = "";
 
+  for (let i of seasons[seasonId].episodes) {
+    console.log(i.title);
+
+    let div = document.createElement("div");
+    div.setAttribute("id", i.id);
+    div.className = "episode";
+    let title = document.createElement("h2");
+    let number = document.createElement("h3");
+
+    number.textContent = `Episode ${i.episodeNumber}`;
+    title.textContent = i.title;
+
+    div.append(number, title);
+    container.appendChild(div);
+
+    document.getElementById(i.id).style.backgroundImage = `url(${i.img})`;
+  }
+}
+
+async function getData() {
+  try {
+    let myRequest = await fetch("data.json");
+
+    let show = await myRequest.json();
+
+    let seasonsInput = document.getElementById("season");
+
+    let seasonId = +seasonsInput.value - 1;
+
+    seasonsInput.addEventListener("input", () => {
+      seasonId = +seasonsInput.value - 1;
+      renderEpisodes(show.seasons, seasonId);
+    });
+
+    renderEpisodes(show.seasons, seasonId);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getData();
